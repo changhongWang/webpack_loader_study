69bef484728fddba05ad82d83876869be4e6f53e
changhong <changhongwang@126.com>
2021-09-06 12:03:07 +0800

    feat: 图片打包loader

--- loaders/spritesLoader.js
+++ loaders/spritesLoader.js
@@ -3,14 +3,21 @@
  * @Author: changhong.wang
  * @Date: 2021-09-05 10:48:58
  * @LastEditors: changhong.wang
- * @LastEditTime: 2021-09-05 13:57:29
+ * @LastEditTime: 2021-09-06 11:59:09
  */
 const Spritesmith = require('spritesmith');
 const path = require('path');
 const sprites = [path.join(__dirname, '../assets/A.png'), path.join(__dirname, '../assets/B.png')];
 
 module.exports = function(source) {
-    console.log('enter spritesLoader.', source)
+    console.log('enter spritesLoader.');
+    // 匹配正则表达式 拿到source中需要打成sprite的部分
+    const results = source.match(/URL\((\S*)\?__sprite/g);
+    const sprites = results.map(item => {
+        const imgPath = item.match(/URL\("(\S*)\?__sprite/)[1];
+        console.log(item, imgPath, 999);
+        return path.join(__dirname, imgPath);
+    })
     const that = this;
     Spritesmith.run({ src: sprites }, function handleRes(err, res) {
         if (err) {
--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader',
+                    path.resolve('./loaders/spritesLoader.js'),
                     'css-loader'
                 ]
             }
