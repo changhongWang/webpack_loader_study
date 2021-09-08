6380c7911f76951efa7d07d96b21771e13583557
changhong <changhongwang@126.com>
2021-09-09 06:00:47 +0800

    feat: 在node中测试jsZip

--- zipTest/ziptest.js
+++ zipTest/ziptest.js
@@ -1,5 +1,14 @@
 const JSZip = require('jszip');
+const fs = require('fs');
 
 const zip = new JSZip();
 
 zip.file("Hello.txt", "Hello World\n");
+zip.generateAsync({type:"nodebuffer"}).then(function(content) {
+    fs.writeFile('example.zip', content, {}, (err, res) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(res);
+    });
+});
