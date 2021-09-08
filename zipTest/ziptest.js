91a7e1c4bc908d27fcb89ed0f8132707287adcec
changhong <changhongwang@126.com>
2021-09-09 06:44:25 +0800

    test: ziptest

--- zipTest/ziptest.js
+++ zipTest/ziptest.js
@@ -1,14 +1,18 @@
 const JSZip = require('jszip');
 const fs = require('fs');
+const path = require('path')
 
 const zip = new JSZip();
 
+console.log('当前path.resolve', path.resolve())
+console.log('当前process.cwd ', process.cwd())
+
 zip.file("Hello.txt", "Hello World\n");
 zip.generateAsync({type:"nodebuffer"}).then(function(content) {
-    fs.writeFile('example.zip', content, {}, (err, res) => {
+    fs.writeFile(path.join(__dirname, 'example.zip'), content, {}, (err, res) => {
         if (err) {
             console.log(err);
         }
-        console.log(res);
+        console.log(res, 5);
     });
 });
